8aa9812bbfc5fb9cf581c625eed303a6db3adb6b
Jon Netzky <jdnetzky@gmail.com>
2020-03-24 17:46:10 -0500

    debug

--- 5ecampaign/src/app.tsx
+++ 5ecampaign/src/app.tsx
@@ -4,6 +4,8 @@ import {Posts} from './posts';
 
 window.onload = () => {
   console.log("LOADING D and D APP");
+  console.log("LOADING...");
   var appNode = document.querySelector('.posts-app');
   ReactDOM.render(<Posts />, appNode);
+  console.log("DONE");
 };
