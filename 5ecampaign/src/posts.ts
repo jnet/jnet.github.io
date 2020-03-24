ea71cb4d868dfd20168dc48d2e1e21a2addda3a4
Jon Netzky <jdnetzky@gmail.com>
2020-03-24 14:06:10 -0500

    added posts

---
+++ 5ecampaign/src/posts.ts
@@ -0,0 +1,14 @@
+
+export interface Post {
+  title : string;
+  post : string;
+  date : string;
+}
+
+export const posts : Post[] = [
+  {
+    title : "Welcome",
+    post : "A quick site I put up to keep everyone up to date with the campaign",
+    date : "3/24/2020"
+  }
+];
